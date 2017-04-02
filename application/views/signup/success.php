624cf136b38d4a33d901507f6335d2410dc2bf09
khyrul-alam <khyrulalam69@gmail.com>
2017-04-03 01:04:37 +0600

    image file path

---
+++ application/views/signup/success.php
@@ -0,0 +1,14 @@
+<div id="login-page">
+    <div class="container">
+
+        <div id="showtime"></div>
+
+            <div class="signup-success">
+                <h2>" CONGRATULATION "</h2>
+                <br>
+                <h3>Your Account has been successfully save. </h3>
+                <a href="<?php echo base_url() ?>">Now You May log-in Here</a>
+            </div>
+
+    </div>
+</div>
